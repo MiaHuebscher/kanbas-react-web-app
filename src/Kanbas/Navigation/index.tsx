4ca85af7820f41f7db7ddcc94021298a2cca3e93
Mia Huebscher <m.huebscher@att.net>
2024-05-18 15:03:42 -0700

    add icons to navigation

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,8 +1,11 @@
 import { AiOutlineDashboard } from "react-icons/ai";
-import { IoCalendarOutline } from "react-icons/io5";
+import { IoCalendarOutline, IoHelpCircleOutline } from "react-icons/io5";
+import { TbArrowLeftFromArc } from "react-icons/tb";
+import { PiShareNetwork } from "react-icons/pi";
+import { GoClock } from "react-icons/go";
 import { SlPeople } from "react-icons/sl";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { FaInbox, FaRegCircleUser, FaArrowRightFromBracket, FaNetworkWired } from "react-icons/fa6";
 
 export default function KanbasNavigation() {
     return (
@@ -26,13 +29,46 @@ export default function KanbasNavigation() {
                         bg-black text-center border-0"> 
               <LiaBookSolid className="fs-1 text-danger" /><br />
               Courses</a>
-            <li><a id="wd-groups-link" href="#/Kanbas/Groups">Groups</a></li>
-            <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
-            <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
-            <li><a id="wd-history-link" href="#/Kanbas/History">History</a></li>
-            <li><a id="wd-commons-link" href="#/Kanbas/Commons">Commons</a></li>
-            <li><a id="wd-studio-link" href="#/Kanbas/Studio">Studio</a></li>
-            <li><a id="wd-help-link" href="#/Kanbas/Help">Help</a></li>
-            <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
+            <a id="wd-groups-link" href="#/Kanbas/Groups"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <SlPeople className="fs-1 text-danger" /><br />
+              Groups</a>
+            <a id="wd-calendar-link" href="#/Kanbas/Calendar"
+              className="list-group-item text-white
+                        bg-black text-center border-0"> 
+              <IoCalendarOutline className="fs-1 text-danger" /><br />
+              Calendar</a>
+            <a id="wd-inbox-link" href="#/Kanbas/Inbox"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <FaInbox className="fs-1 text-danger"/><br />
+              Inbox</a>
+            <a id="wd-history-link" href="#/Kanbas/History"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <GoClock className="fs-1 text-danger"/><br />
+              History</a>
+            <a id="wd-commons-link" href="#/Kanbas/Commons"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <FaArrowRightFromBracket className="fs-1 text-danger"/><br />  
+              Commons</a>
+            <a id="wd-studio-link" href="#/Kanbas/Studio"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <PiShareNetwork className="fs-1 text-danger" /><br />
+              Studio</a>
+            <a id="wd-help-link" href="#/Kanbas/Help"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <IoHelpCircleOutline className="fs-1 text-danger" /><br />
+              Help</a>
+            <a id="wd-labs-link" href="#/Labs"
+              className="list-group-item text-white
+                        bg-black text-center border-0">
+              <LiaCogSolid className="fs-1 text-danger"/><br />
+              Labs</a>
+        </div>
     )
 }
