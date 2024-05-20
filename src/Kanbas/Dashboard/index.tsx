a6a6ea3721e385da0e1b73e1d78f0b68f4597fd4
Mia Huebscher <m.huebscher@att.net>
2024-05-19 21:27:42 -0700

    update course links

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -12,7 +12,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    CS5610 Web Development
+                                    CS5610 Web Development SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Web Development
@@ -28,7 +28,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    CK2500 Intermediate Cooking
+                                    CK2500 Intermediate Cooking SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Intermediate Cooking
@@ -44,7 +44,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    SU4500 Advanced Surfing
+                                    SU4500 Advanced Surfing SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Advanced Surfing
@@ -60,7 +60,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    ARTD4660 Advanced Photography
+                                    ARTD4660 Advanced Photography SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Advanced Photography
@@ -76,7 +76,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    BIOL1111 General Biology
+                                    BIOL1111 General Biology SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     General Biology
@@ -92,7 +92,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    DS4400 Machine Learning and Data Mining 1
+                                    DS4400 Machine Learning and Data Mining 1 SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Machine Learning and Data Mining 1
@@ -108,7 +108,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    HK7500 Extreme Hiking
+                                    HK7500 Extreme Hiking SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Extreme Hiking
@@ -124,7 +124,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    GM6000 Advanced German
+                                    GM6000 Advanced German SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Advanced German
@@ -140,7 +140,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    PM1000 The Art of Pretzel Making
+                                    PM1000 The Art of Pretzel Making SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     The Art of Pretzel Making
@@ -156,7 +156,7 @@ export default function Dashboard() {
                                 <a className="wd-dashboard-course-link"
                                     href="#/Kanbas/Courses/1234/Home"
                                     style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    DS4300 Large-Scale Information Storage and Retrieval
+                                    DS4300 Large-Scale Information Storage and Retrieval SEC 01 Summer 1 2024
                                 </a>
                                 <p className="wd-dashboard-course-title card-text">
                                     Large-Scale Information Storage and Retrieval
