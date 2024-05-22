93ee9d0718ab39ba126245923e96f62fdcdf6982
Mia Huebscher <m.huebscher@att.net>
2024-05-22 12:13:49 -0700

    implement JavaScript for Lab3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,8 +1,16 @@
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
-    return (
-      <div className="container">
-        <h2>Lab 3</h2>
-      </div>
-    );
-  }
-  
+  return(
+    <div id="wd-lab3" className="container-fluid">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/><br />
+      <VariableTypes /><br />
+      <BooleanVariables /><br />
+      <IfElse /><br />
+    </div>
+  );
+}
