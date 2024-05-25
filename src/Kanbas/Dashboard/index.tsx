bf59119dc90322a033fdc63738242acae0a2bf7c
Mia Huebscher <m.huebscher@att.net>
2024-05-25 12:56:47 -0700

    make the dashboard data-driven

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,175 +1,37 @@
+import { Link } from "react-router-dom";
+import * as db from "../Database";
 import "./index.css"
 
 export default function Dashboard() {
+    const courses = db.courses;
     return (
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-            <h2 id="wd-dashboard-published">Published Courses (10)</h2> <hr />
+            <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses" className="row">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/webdev.jpg" alt="web development icon"/>
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    CS5610 Web Development SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Web Development
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
+                    {courses.map((course) => (
+                        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+                            <Link to={`/Kanbas/Courses/${course._id}/Home`} className="text-decoration-none">
+                                <div className="card rounded-3 overflow-hidden">
+                                    <img src={`/images/${course.image}`} height="{160}" />
+                                    <div className="card-body">
+                                        <span className="wd-dashboard-course-link"
+                                            style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }} >
+                                            {course.name}
+                                        </span>
+                                        <p className="wd-dashboard-course-title card-text" style={{ maxHeight: 53, overflow: "hidden" }}>
+                                            {course.description}
+                                        </p>
+                                        <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                                    </div>
+                                </div>
+                            </Link>
                         </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/cooking.jpg" alt="cookin icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    CK2500 Intermediate Cooking SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Intermediate Cooking
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/surfing.jpg" alt="surfing icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    SU4500 Advanced Surfing SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Advanced Surfing
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/photography.jpg" alt="photography icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    ARTD4660 Adv. Photography SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Advanced Photography
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/biology.jpg" alt="biology icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    BIOL1111 General Biology SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    General Biology
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/ml.jpg" alt="machine learning icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    DS4400 Machine Learning SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Machine Learning + Data Mining
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/hiking.jpg" alt="hiking icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    HK7500 Extreme Hiking SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Extreme Hiking
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/german.jpg" alt="germany icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    GM6000 Advanced German SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Advanced German
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/pretzel.jpg" alt="pretzel icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    PM1000 The Art of Pretzel Making SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    The Art of Pretzel Making
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <img src="/images/database.jpg" alt="database icon" />
-                            <div className="card-body">
-                                <a className="wd-dashboard-course-link"
-                                    href="#/Kanbas/Courses/1234/Home"
-                                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                    CS3200 Database Design SEC 01 Summer 1 2024
-                                </a>
-                                <p className="wd-dashboard-course-title card-text">
-                                    Database Design
-                                </p>
-                                <a href="#/Kanbas/Courses/1234/Home" className="btn btn-primary"> Go </a>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
-        <br /><br />
+            <br />
         </div>
-    )
+    );
 }
