802053ef01ba9335d6e198e4e3f20484d366941b
Mia Huebscher <m.huebscher@att.net>
2024-06-08 13:47:19 -0700

    finish code for lab 5

--- src/Labs/Lab 5/client.ts
+++ src/Labs/Lab 5/client.ts
@@ -12,11 +12,6 @@ export const fetchAssignment = async () => {
     return response.data;
 };
 
-export const updateTitle = async (title: string) => {
-    const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
-    return response.data;
-};
-
 const TODOS_API = `${REMOTE_SERVER}/lab5/todos`;
 export const fetchTodos = async () => {
     const response = await axios.get(TODOS_API);
@@ -28,7 +23,28 @@ export const removeTodo = async (todo: any) => {
     return response.data;
 };
 
+export const deleteTodo = async (todo: any) => {
+    const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+    return response.data;
+};  
+
 export const createTodo = async () => {
     const response = await axios.get(`${TODOS_API}/create`);
     return response.data;
 };
+
+export const postTodo = async (todo: any) => {
+    const response = await axios.post(`${TODOS_API}`, todo);
+    return response.data;
+};
+
+export const updateTitle = async (title: string) => {
+    const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
+    return response.data;
+};
+
+export const updateTodo = async (todo: any) => {
+    const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
+    return response.data;
+  };
+  
