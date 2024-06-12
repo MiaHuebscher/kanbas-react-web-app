97b86264626e2037d5e98d0f7bbd9cd6f1d4e0af
Mia Huebscher <m.huebscher@att.net>
2024-06-12 10:40:40 -0700

    implement account controls

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -3,15 +3,21 @@ import { Link, useNavigate } from "react-router-dom";
 import * as client from "./client";
 
 export default function Signin() {
+  const [error, setError] = useState("");
   const [credentials, setCredentials] = useState<any>({});
   const navigate = useNavigate();
   const signin = async () => {
-    await client.signin(credentials);
-    navigate("/Kanbas/Account/Profile");
+    try {
+      await client.signin(credentials);
+      navigate("/Kanbas/Account/Profile");
+    } catch (err: any) {
+      setError(err.response.data.message);
+    }
   };
   return (
     <div>
       <h1>Sign in</h1>
+      {error && <div className="alert alert-danger">{error}</div>}
       <input onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
         value={credentials.username} className="form-control mb-2" placeholder="username" />
       <input onChange={(e) => setCredentials({ ...credentials, password: e.target.value }) }
--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import * as client from "./client";
 export default function Signup() {
+  const [error, setError] = useState("");
   const [user, setUser] = useState<any>({});
   const navigate = useNavigate();
   const signup = async () => {
-    await client.signup(user);
-    navigate("/Kanbas/Account/Profile");
+    try {
+      user._id = new Date().getTime().toString();
+      await client.signup(user);
+      navigate("/Kanbas/Account/Profile");
+    } catch (err: any) {
+      setError(err.response.data.message);
+    }
   };
   return (
     <div>
       <h1>Sign up</h1>
+      {error && <div className="alert alert-danger">{error}</div>}
       <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })}
              className="form-control mb-2" placeholder="username" />
       <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} type="password"
