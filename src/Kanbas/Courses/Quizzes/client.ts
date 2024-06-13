9ec7d2d92b9d2164a9e9f1e2e96d951f08e58511
Mia Huebscher <m.huebscher@att.net>
2024-06-12 19:06:35 -0700

    Update client.ts

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -1,25 +1,26 @@
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
 
 
 export const findQuizzesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/quizzes`);
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/quizzes`);
   return response.data;
 };
 
 export const createQuiz = async (courseId: string, quiz: any) => {
-  const response = await axios.post( `${COURSES_API}/${courseId}/quizzes`, quiz );
+  const response = await axiosWithCredentials.post( `${COURSES_API}/${courseId}/quizzes`, quiz );
   return response.data;
 };
 
 export const deleteQuiz = async (quizId: string) => {
-  const response = await axios.delete(`${QUIZZES_API}/${quizId}`);
+  const response = await axiosWithCredentials.delete(`${QUIZZES_API}/${quizId}`);
   return response.data;
 };
 
 export const updateQuiz = async (quiz: any) => {
-  const response = await axios.put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  const response = await axiosWithCredentials.put(`${QUIZZES_API}/${quiz._id}`, quiz);
   return response.data;
 };
