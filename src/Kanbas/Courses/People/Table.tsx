a9c44b9d595f4f4dbba9aa965df1a87c5fb9bb13
Mia Huebscher <m.huebscher@att.net>
2024-06-24 11:47:57 -0700

    Update Table.tsx

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -3,10 +3,12 @@ import { FaPlus } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import * as client from "./client";
 import PeopleDetails from "./Details";
+import { useSelector } from "react-redux";
 
 export default function PeopleTable() {
   const { cid } = useParams();
   const [users, setUsers] = useState<any[]>([]);
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
   const [role, setRole] = useState("");
   const [name, setName] = useState("");
   const fetchUsers = async () => {
@@ -49,10 +51,11 @@ export default function PeopleTable() {
   }, []);
   return (
     <div id="wd-people-table">
+      {(currentUser.role === "FACULTY" || currentUser.role === "TA") &&
       <button onClick={createUser} className="float-end btn btn-danger">
         <FaPlus className="me-2" />
         People
-      </button>
+      </button>}
       <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
              className="form-control float-start w-25 me-2" />
       <select value={role} onChange={(e) =>filterUsersByRole(e.target.value)} className="form-select float-start w-25" >
@@ -70,11 +73,12 @@ export default function PeopleTable() {
           {users.map((user: any) => (
             <tr key={user._id}>
               <td className="text-nowrap"> 
+                {currentUser.role === "FACULTY" || currentUser.role === "TA" ?
                 <Link to={`/Kanbas/Courses/${cid}/People/${user._id}`} 
                       className="text-danger text-decoration-none"  >
                       <PeopleDetails fetchUsers={fetchUsers} />
                   {user.firstName} {user.lastName}
-                </Link> 
+                </Link> : <span className="text-danger">{user.firstName} {user.lastName}</span>} 
               </td>
               <td>{user._id}</td><td>{user.section}</td><td>{user.role}</td><td>{user.lastActivity}</td>
               <td>{user.totalActivity}</td>
