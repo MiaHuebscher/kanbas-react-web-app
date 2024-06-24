91134fa274332063ce17d9d45aad713f944feddd
Mia Huebscher <m.huebscher@att.net>
2024-06-24 12:42:04 -0700

    Update client.ts

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -10,6 +10,7 @@ export const signin = async (credentials: any) => {
 
 export const profile = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+  console.log("Profile response:", response);
   return response.data;
 };
 
